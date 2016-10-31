37ea48b59afeab48a13779baccfd6102919f303e
Nigel Barber <nigel@mindbrix.co.uk>
2016-10-31 19:50:21 +0000

    Added clear button

--- Symmetrix/ViewController.swift
+++ Symmetrix/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var canvasView: SymmetrixView?
+    
+    @IBAction func clearButtonTapped(item: UIBarButtonItem) {
+        self.canvasView!.clear()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
